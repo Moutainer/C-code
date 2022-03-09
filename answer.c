7bf1084a4a84c2d34395ef7137ae8ef51a32cf21
Moutainer <88609851+Moutainer@users.noreply.github.com>
2022-03-09 10:20:13 +0800

    Add files via upload
    C程序设计第一次课程课堂练习题-二维数组的输出

--- answer.c
+++ answer.c
@@ -1,67 +1,3 @@
-#include<stdio.h>
-int main()
-{
-	int a[5][4];
-	for (int i = 0; i < 5; i++)
-	{
-		printf("Enter No. and score as: MT EN PH\n");
-		for (int j = 0; j < 4; j++)
-		{
-			scanf("%d", &a[i][j]);
-		}
-	}
-	int sum[5] = { 0 };
-	for (int i = 0; i < 5; i++)
-	{
-		for (int j = 1; j < 4; j++)
-		{
-			sum[i] += a[i][j];
-		}
-	}
-	int v[5] = { 0 };
-	for (int i = 0; i < 5; i++)
-	{
-		v[i] = sum[i] / 3;
-	}
-	char bool[5] = { 'N','N','N','N','N'};
-	for (int i = 0; i < 5; i++)
-	{
-		
-		if (a[i][1] > 90 && a[i][2] >90&&a[i][3]>90)
-		{
-			 bool[i] = 'Y';
-		}
-		}
-	for (int i = 0; i < 5; i++)
-	{
-		printf("%c",bool[i]);
-	}
-	printf("NO\tMT\tEN\tPH\tSUM\tV\t>90\n");
-	printf("------------------------------------------------------\n");
-	int result[5][7] = { 0 };
-	for (int i = 0; i < 5; i++)
-	{
-		for (int j = 0; j < 7; j++)
-		{	
-			if (j == 4)
-			{
-				printf("%d\t",sum[i]);
-			}
-			else if (j == 5)
-			{
-				printf("%d\t", v[i]);
-
-			}
-			else if (j == 6)
-			{
-				printf("%c\n",bool[i]);
-			}else{
-				printf(" % d\t", a[i][j]);
-			}
-		}
-	}
-	return 0;
-}
 /*
 普通编程题(10分)
 1.(Q63, 10分) 某班期终考试科目为高等数学MT，英语EN和物理PH，有5人参加考试。为评定奖学金，要求统计并输出一个表格，表格内容包括学号、各科分数、总分和平均分，并标出三门课均在90分以上者（该栏标志输出“Y”，否则输出“N”），表格形式如下：
@@ -97,3 +33,37 @@ NO      MT      EN      PH      SUM     V       >90
 4       66      89      86      241     80      N
 5       97      86      88      271     90      N
 */
+#include<stdio.h>
+int main()
+{
+	int a[5][4];
+	char c;
+	int s = 0;
+	int j = 1;
+	for (int i = 0; i < 5; i++)
+	{
+		printf("Enter No. and score as: MT EN PH\n");
+		for (int j = 0; j < 4; j++)
+		{
+			scanf("%d", &a[i][j]);
+		}
+	}
+	printf("NO\tMT\tEN\tPH\tSUM\tV\t>90\n");
+	printf("------------------------------------------------------\n");
+	for (int i = 0; i < 5; i++)
+	{
+		printf("%d\t",a[i][0]);
+		for ( s = 0, j =1; j < 4; j++)
+		{
+			s += a[i][j];
+			printf("%d\t",a[i][j]);
+		}
+		if (a[i][1] > 90 && a[i][2] > 90 && a[i][3] > 90)
+		{
+			c = 'Y';
+		}
+		else c = 'N';
+		printf("%d\t%d\t%c\n",s,s/3,c);
+	}
+	return 0;
+}
